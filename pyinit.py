46dad6c2edd060621f7bcd68e896cb796c24fb1d
Archie Collison <archie.collison@gmail.com>
2022-08-15 22:26:47 +1000

    Updated framework

--- pyinit.py
+++ pyinit.py
@@ -1,22 +1,22 @@
-import argparse
 import os
+import argparse
 
 # set up parser
-parser = argparse.ArgumentParser(description='Set up a project folder')
-args = parser.parse_args()
-
-# create a folder in Users/archie/repos/
-parser.add_argument('cd')
-
+parser = argparse.ArgumentParser(prog='pyinit', description='set up a project folder')
 
 # parent dir
 parent = '/Users/archie/repos/'
 
+# create a folder in Users/archie/repos/
+parser.add_argument('d', help = 'create a new directory here')
 
+# parse arguments after creating them
+args = parser.parse_args()
 
 def main():
-    print(parent)
+    child = parent + args.d # created with args, must come after parsing.
 
+    print(child)
 
 if __name__ == "__main__":
     main()

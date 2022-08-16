88996e3f70973bffd2e430be797d09829beb9072
Archie Collison <archie.collison@gmail.com>
2022-08-16 19:06:10 +1000

    basic template functionality working

--- pyinit.py
+++ pyinit.py
@@ -1,28 +1,28 @@
 import os
 import argparse
 import template
-# set up parser
+# set up parser and args
 parser = argparse.ArgumentParser(prog='pyinit', description='create a project folder')
 
-#parser.add_argument('c', type=str, help='create a new directory here')
+parser.add_argument('c', type=str, help='create a new directory here')
 parser.add_argument('-p', '--parent', type=str, help='change the parent directory') # optional arg last
 parser.add_argument('-t', '--template', type=str, help='set the template for the project')
+args = parser.parse_args()
+
+def template_maker(type):
+    for i in template.typedict[type]:
+        open(i, 'a')
+        print('created ' + i)
 
 def main():
-    args = parser.parse_args() # parse arguments after creating them
-    parent = '/Users/archie/repos/'
-    
-    if args.parent:
-        parent = args.parent
-    if args.template:
-        type = args.template
-        for i in template.website:
-            print(template.website[i])
-    #child = parent+args.c # must come after parse_args
 
-    #print('created '+child)
+    parent = args.parent if args.parent else '/Users/archie/repos/' # if new parent, assign
+    child = parent+args.c
+    if args.template: # if template, call maker
+        template_maker(str(args.template))
 
-    #os.mkdir(child)
+    print('created '+child)
+    os.mkdir(child)
 
 if __name__ == "__main__":
     main()
